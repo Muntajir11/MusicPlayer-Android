23d259f40924fe9ff4fc7f93432e991d429f02a3
Muntajir Hussain Gazi <muntajirwork11@gmail.com>
2024-12-05 22:18:17 +0530

    unreleased version

--- index.js
+++ index.js
@@ -3,7 +3,14 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App';
+import TrackPlayer from 'react-native-track-player';
+import { playbackService } from './musicPlayerServices';
+
 import {name as appName} from './app.json';
 
+
+console.log('Registering app');
 AppRegistry.registerComponent(appName, () => App);
+
+TrackPlayer.registerPlaybackService(() => playbackService);
---
+++ src/App.tsx
@@ -0,0 +1,77 @@
+import React, {useState, useEffect} from "react"
+
+import type {PropsWithChildren} from 'react';
+import {
+  ActivityIndicator,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {setupPlayer, addTrack} from "../musicPlayerServices"
+import MusicPlayer from "./screens/MusicPlayer";
+
+
+function App(): JSX.Element {
+  const [isPlayerReady, setIsPaylerReady] = useState(false)
+
+  async function setup(){
+    let isSetup = await setupPlayer()
+
+    if (isSetup) {
+      await addTrack()
+    }
+
+    setIsPaylerReady(isSetup)
+  }
+
+  useEffect(() => {
+    setup()
+  }, [])
+  
+  if (!isPlayerReady) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle={"light-content"} />
+      <MusicPlayer />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1
+  }
+});
+
+export default App;
+
+// import { View, Text, StyleSheet } from 'react-native'
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <View>
+//       <Text>App</Text>
+//     </View>
+//   )
+// }
+
+// const styles = StyleSheet.create({
+//   container:{
+//     flex: 1
+//   }
+// });
+
+// export default App
